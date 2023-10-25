c8fa506e8b9c822aa64b39be7280d0c92f508eb0
emirsafayavuz <emirsafayavuz@gmail.com>
2023-10-25 20:48:13 +0300

    Cors Added

--- Program.cs
+++ Program.cs
@@ -3,9 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCorsPolicy",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -19,6 +28,9 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors("MyCorsPolicy");
+
 app.UseDeveloperExceptionPage();
 
 app.UseHttpsRedirection();
